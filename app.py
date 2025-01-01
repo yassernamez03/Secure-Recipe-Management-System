d1770c4103f94d1857d27baa876a2f38b76be0c3
YasserNamez <99099773+yassernamez03@users.noreply.github.com>
2025-01-01 14:57:44 +0100

    0.0.1

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-import os 
+import os
 import re
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
-from flask import jsonify
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'recipes_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@my-first-cluster-hgde0.mongodb.net/recipes_manager?retryWrites=true'
 
-mongo = PyMongo(app)
-
+# MongoDB connection
+client = MongoClient('mongodb+srv://namezyasser3:admin@cluster0.ga0p0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+db = client.recipes_manager
 
 # -----Homepage------
 
@@ -24,32 +22,30 @@ def get_recipes():
         preparationtime = None
         categoryname = None
         
-        if request.args.get('recipe_name') is not None and request.args.get('recipe_name') is not '':
+        if request.args.get('recipe_name') is not None and request.args.get('recipe_name') != '':
             recipenameregex = "\W*"+request.args.get("recipe_name")+"\W*"
             recipename = re.compile(recipenameregex, re.IGNORECASE)
           
-        if request.args.get('preparation_time') is not None and request.args.get('preparation_time') is not '':
+        if request.args.get('preparation_time') is not None and request.args.get('preparation_time') != '':
             preparationtimeregex = "\W*"+request.args.get("preparation_time")+"\W*"
             preparationtime = re.compile(preparationtimeregex, re.IGNORECASE)
         
-        if request.args.get('category_name') is not None and request.args.get('category_name') is not '':
+        if request.args.get('category_name') is not None and request.args.get('category_name') != '':
             categoryregex = "\W*"+request.args.get("category_name")+"\W*"
             categoryname = re.compile(categoryregex, re.IGNORECASE)
-            
 
-        recipes=mongo.db.recipes.find( { "$or": [{"recipe_name": recipename}, {"preparation_time": preparationtime}, {"category_name": categoryname}] } )
-        return render_template("recipes.html", recipes=recipes, categories=mongo.db.categories.find()) 
+        # Accessing the 'recipes' collection and querying the database
+        recipes = db.recipes.find({ "$or": [{"recipe_name": recipename}, {"preparation_time": preparationtime}, {"category_name": categoryname}] })
+        return render_template("recipes.html", recipes=recipes, categories=db.categories.find())
         
-    return render_template("recipes.html", recipes=mongo.db.recipes.find(), categories=mongo.db.categories.find())
-    
-    
+    return render_template("recipes.html", recipes=db.recipes.find(), categories=db.categories.find())
 
 # -----Charts------
 @app.route('/charts')
 def charts():
     results = {"labels": [ ], "data": [ ]}
-    categories = mongo.db.categories
-    recipes = mongo.db.recipes
+    categories = db.categories
+    recipes = db.recipes
     all_categories = categories.find({})
     for category in all_categories:
         category_counts = recipes.find({"category_name": category["category_name"]}).count()
@@ -57,104 +53,90 @@ def charts():
         results["data"].append(category_counts)
     
     return render_template("charts.html", results=results)
-    
-    
+
 # -----Add Recipe------
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('addrecipe.html',
-                           categories=mongo.db.categories.find())
-
+    return render_template('addrecipe.html', categories=db.categories.find())
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    recipes = mongo.db.recipes
+    recipes = db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
-
 # -----Edit Recipe------
-
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
-    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
-    all_categories =  mongo.db.categories.find()
-    return render_template('editrecipe.html', recipe=the_recipe,
-                           categories=all_categories)
-
+    the_recipe = db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
 
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
-    recipes = mongo.db.recipes
-    recipes.update( {'_id': ObjectId(recipe_id)},
+    recipes = db.recipes
+    recipes.update_one({'_id': ObjectId(recipe_id)},
     {
-        'recipe_name':request.form.get('recipe_name'),
-        'category_name':request.form.get('category_name'),
-        'recipe_intro':request.form.get('recipe_intro'),
-        'ingredients': request.form.get('ingredients'),
-        'description': request.form.get('description'),
-        'preparation_time': request.form.get('preparation_time'),
-        'photo_url': request.form.get('photo_url')
-        
+        '$set': {
+            'recipe_name': request.form.get('recipe_name'),
+            'category_name': request.form.get('category_name'),
+            'recipe_intro': request.form.get('recipe_intro'),
+            'ingredients': request.form.get('ingredients'),
+            'description': request.form.get('description'),
+            'preparation_time': request.form.get('preparation_time'),
+            'photo_url': request.form.get('photo_url')
+        }
     })
     return redirect(url_for('get_recipes'))
 
-
 # -----Delete Recipe------
-
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
-    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    db.recipes.delete_one({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
 
-
 # -----Categories funcitionalities------
-
 @app.route('/categories')
 def categories():
-    return render_template('categories.html',
-                           categories=mongo.db.categories.find())
-                           
+    return render_template('categories.html', categories=db.categories.find())
+
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
-                           category=mongo.db.categories.find_one(
-                           {'_id': ObjectId(category_id)}))                           
+                           category=db.categories.find_one({'_id': ObjectId(category_id)}))
 
 @app.route('/update_category/<category_id>', methods=['POST'])
 def update_category(category_id):
-    mongo.db.categories.update(
+    db.categories.update_one(
         {'_id': ObjectId(category_id)},
-        {'category_name': request.form.get('category_name')})
+        {'$set': {'category_name': request.form.get('category_name')}}
+    )
     return redirect(url_for('categories'))
 
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    db.categories.delete_one({'_id': ObjectId(category_id)})
     return redirect(url_for('categories'))
-    
+
 @app.route('/insert_category', methods=['POST'])
 def insert_category():
     category_doc = {'category_name': request.form.get('category_name')}
-    mongo.db.categories.insert_one(category_doc)
+    db.categories.insert_one(category_doc)
     return redirect(url_for('categories'))
 
 @app.route('/add_category')
 def add_category():
     return render_template('addcategory.html')
-   
-    
-# -----Single Page Recipe------
 
+# -----Single Page Recipe------
 @app.route('/recipe_single/<recipe_id>')
 def recipe_single(recipe_id):
     return render_template("recipepage.html",
-                           recipes=mongo.db.recipes.find({'_id': ObjectId(recipe_id)}))
-                           
+                           recipes=db.recipes.find({'_id': ObjectId(recipe_id)}))
 
 # ************************************************
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+            port=int(os.environ.get('PORT', 5000)),
             debug=True)
